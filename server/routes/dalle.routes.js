35825b095b9c5ca919fdc9e729cad436ebdffdeb
Sarvani Ankala <ankaladurgasarvani@gmail.com>
2024-07-08 00:48:09 +0530

    OPEN_KEY

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -21,6 +21,7 @@ router.route('/').post(async (req, res) => {
     try {
 
         const { prompt } = req.body;
+        console.log('Prompt received:', prompt);
 
         const response = await openai.createImage({
             prompt,
@@ -29,13 +30,15 @@ router.route('/').post(async (req, res) => {
             response_format: 'b64_json'
         })
 
+        console.log('Response from OpenAI:', response.data);
+
         const image = response.data.data[0].b64_json
 
         res.status(200).json({ photo: image })
     } 
     catch (error) {
-        console.error(error)
-        res.status(500).json({ message: "Something went wrong"})
+        console.error('Error creating image:',error)
+        res.status(500).json({ message: "Something went wrong", error: error.message})
     }
 })
 
